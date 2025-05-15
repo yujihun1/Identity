c190601c5a9f019258ed5ea60377c1c782ef274c
yujihun <sk0253642@gmail.com>
2025-05-15 17:15:04 +0900

    Identitiy pk  change

--- IdentityTest/Controllers/HomeController.cs
+++ IdentityTest/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using IdentityTest.Data;
 using IdentityTest.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,15 +7,18 @@ namespace IdentityTest.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
         public IActionResult Index()
         {
+            var user = _dbContext.Users.FirstOrDefault();
             return View();
         }
 
--- IdentityTest/Program.cs
+++ IdentityTest/Program.cs
@@ -1,4 +1,5 @@
 using IdentityTest.Data;
+using IdentityTest.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,8 +11,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
